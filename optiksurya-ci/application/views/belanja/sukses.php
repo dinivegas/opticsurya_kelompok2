3a971815d1f35e48bb676906662768e524ba6eed
elisaqothrun <elisaqothrunnada88@gmail.com>
2020-06-14 21:54:51 +0700

    checkout

--- optiksurya-ci/application/views/belanja/sukses.php
+++ optiksurya-ci/application/views/belanja/sukses.php
@@ -1,5 +1,5 @@
 <!-- Shopping Cart Section Begin --> 
-<section class="shopping-cart spad">
+<section class="cart bgwhite p-t-70 p-b-100">
 <div class="container">
 <h2><?php echo $title ?></h2>
 <br>
@@ -9,10 +9,8 @@
     echo '</div>';
 }
 ?>
-<p class="alert alert-success">Terimakasih, Produk yang anda beli akan kami proses</p>
+<p class="alert alert-success">Terimakasih, produk yang Anda beli akan segera kami proses <a href="<?php echo base_url('masuk') ?>" class="btn btn-info"> Silahkan lakukan pembayaran </a></p>
 <div class="col-md-12">
-
-	
 </div>
 
 <div class="col-lg-12">
