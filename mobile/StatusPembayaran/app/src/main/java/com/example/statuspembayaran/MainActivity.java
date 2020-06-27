907d162e89611bf1ad214abbef8f9699627692c5
thisisalfarissitorus <sitorus64bit@gmail.com>
2020-06-27 18:50:05 +0700

    TopBar StatusPembayaran
    Ditambahkan StatusPembayaran TopBar (InProgress)

--- mobile/StatusPembayaran/app/src/main/java/com/example/statuspembayaran/MainActivity.java
+++ mobile/StatusPembayaran/app/src/main/java/com/example/statuspembayaran/MainActivity.java
@@ -2,23 +2,77 @@ package com.example.statuspembayaran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.View;
-
-
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    ListView listView;
+    String[] nameList = {"Kacamata", "Frame", "Lensa", "Gratis"};
+    ArrayAdapter<String> adapter;
+    Context context;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        */
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.ListView);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameList);
+        listView.setAdapter(adapter);
+
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.cari);
+
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Cari Produk");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        /*
+        if (id == R.id.brand) {
+            // do something here
+        }
+        */
+        return super.onOptionsItemSelected(item);
     }
 }
